fdfc98b5652ba087fc592065f30d1cc636bb8760
寺田優 <mterada1228@gmail.com>
2020-06-12 13:43:00 +0900

    add data plot module

--- analytics/make_rap_and_RPCI_plot_from_race_result.py
+++ analytics/make_rap_and_RPCI_plot_from_race_result.py
@@ -2,18 +2,16 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.exc import NoResultFound
 from model.raceResult import RaceResult
+import matplotlib
+import matplotlib.pyplot as plt
 
 import MySQLdb
 
 def main():
-
-    race_result = getRaceResultData()
-    print(race_result)
-
-    # TODO データをプロットする。
+    race_results = getRaceResultData()
+    dataPlot(race_results)
 
 def getRaceResultData():
-
     # DBへ接続する
     # 接続ホスト名: localhost
     # データベース名: netkeiba
@@ -26,11 +24,31 @@ def getRaceResultData():
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    race_result = session.query(RaceResult).all() 
+    race_results = session.query(RaceResult).all() 
 
     session.close()
 
-    return race_result
+    return race_results
+
+def dataPlot(race_results):
+
+    """ グラフを描画する """
+
+    # 日本語フォントを有効にする
+    matplotlib.rcParams['font.sans-serif'] = 'MigMix 1P'    
+
+    # dataをplotする
+    for race_result in race_results:
+        plt.plot(race_result.RPCI, race_result.ave_1F, 'ro', label=race_result.name)
+
+    # グラフの体裁を整える
+    plt.xlabel('RPCI')
+    plt.ylabel('ave-1F')
+    plt.title('RPCI x ave-1F マトリクス')
+    plt.legend(loc='best') # 凡例をいい感じの位置に配置する
+    plt.xlim(40, 70)
+    plt.ylim(10, 14)
+    plt.savefig('test.png', dpi=300)
 
 if __name__ == "__main__":
     main()
