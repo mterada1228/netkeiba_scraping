4738201bc608c15bc2f508bd9fa12af5feea238e
寺田優 <mterada1228@gmail.com>
2020-06-15 19:49:46 +0900

    add plot module

--- analytics/make_rap_and_RPCI_plot_from_hose.py
+++ analytics/make_rap_and_RPCI_plot_from_hose.py
@@ -7,6 +7,8 @@ from model.hoseRaceResult import HoseRaceResult
 import matplotlib
 import matplotlib.pyplot as plt
 
+import pdb
+
 import MySQLdb
 
 def main():
@@ -25,14 +27,18 @@ def main():
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Hose データの取得
-    hoseRaceResults = getHoseRaceResult(session, '2010100690')
+    # HoseRaceResult データの取得
+    hoseRaceResults = getHoseRaceResult(session, '2015103057')
+
+    # RaceResult　データの取得
+    raceResults = getRaceResult(session, "函館スプリント")
 
     # Session クローズ
     session.close()
 
     # グラフにプロットする
-    dataPlot(hoseRaceResults)
+    # dataPlot(hoseRaceResults)
+    dataPlot(hoseRaceResults, raceResults)
 
 def getHoseRaceResult(session, hose_id):
 
@@ -43,25 +49,95 @@ def getHoseRaceResult(session, hose_id):
 
     return hoseRaceResults
 
-def dataPlot(hoseRaceResults):
+def getRaceResult(session, raceName):
+
+    raceResults = session.query(RaceResult).filter(RaceResult.name.like(f'%{raceName}%'))
+
+    return raceResults
+
+def dataPlot(hoseRaceResults, raceResults=None):
 
     """ グラフを描画する """
 
     # 日本語フォントを有効にする
-    matplotlib.rcParams['font.sans-serif'] = 'MigMix 1P'    
+    matplotlib.rcParams['font.sans-serif'] = 'MigMix 1P'   
 
-    # dataをplotする
+    # グラフプロット
+    x = [ hoseRaceResult.RaceResult.RPCI for hoseRaceResult in hoseRaceResults ]
+    y = [ hoseRaceResult.RaceResult.ave_1F for hoseRaceResult in hoseRaceResults ]
+    names = [ f'{hoseRaceResult.RaceResult.date} {hoseRaceResult.RaceResult.name} ' for hoseRaceResult in hoseRaceResults ]
+
+    """ 
+        着差ごとにplotは色分けする 
+        　・1着 -> 黄色
+        　・~0.4s 差 -> 緑
+        　・0.4s~1.0s 差 -> 青
+        　・1.0s〜 差 -> 灰色    
+    """
+
+    colorTbl = []
     for hoseRaceResult in hoseRaceResults:
-        plt.plot(hoseRaceResult.RaceResult.RPCI, hoseRaceResult.RaceResult.ave_1F, 'ro', label=hoseRaceResult.RaceResult.name)
+        if hoseRaceResult.HoseRaceResult.rank == '1':
+            colorTbl.append('yellow')
+        elif float(hoseRaceResult.HoseRaceResult.time_diff) <= 0.4:
+            colorTbl.append('green')
+        elif float(hoseRaceResult.HoseRaceResult.time_diff) <= 1.0:
+            colorTbl.append('blue')
+        else:
+            colorTbl.append('gray')
+
+    # RaceResultsを受け取った場合はプロットする
+    if raceResults != None:
+        for raceResult in raceResults:
+            x.append(raceResult.RPCI)
+            y.append(raceResult.ave_1F)
+            names.append(f'{hoseRaceResult.RaceResult.date} {raceResult.name}')
+            colorTbl.append("red")
+
+    fig,ax = plt.subplots()
+    sc = plt.scatter(x, y, c=colorTbl)
+
+    # プロットにホバーした時、凡例を表示させる
+    annot = ax.annotate("", xy=(0,0), xytext=(20,20),textcoords="offset points",
+                    bbox=dict(boxstyle="round", fc="w"),
+                    arrowprops=dict(arrowstyle="->"))
+
+    annot.set_visible(False)
+
+    def update_annot(ind):
+
+        pos = sc.get_offsets()[ind["ind"][0]]
+        annot.xy = pos
+        text = "{}, {}".format(" ".join(list(map(str,ind["ind"]))), 
+                            " ".join([names[n] for n in ind["ind"]]))
+        annot.set_text(text)
+        annot.get_bbox_patch().set_facecolor(colorTbl[ind["ind"][0]])
+        annot.get_bbox_patch().set_alpha(0.4)
+
+
+    def hover(event):
+        vis = annot.get_visible()
+        if event.inaxes == ax:
+            cont, ind = sc.contains(event)
+            if cont:
+                update_annot(ind)
+                annot.set_visible(True)
+                fig.canvas.draw_idle()
+            else:
+                if vis:
+                    annot.set_visible(False)
+                    fig.canvas.draw_idle()
+
+    fig.canvas.mpl_connect("motion_notify_event", hover)
 
     # グラフの体裁を整える
     plt.xlabel('RPCI')
     plt.ylabel('ave-1F')
-    plt.title('RPCI x ave-1F マトリクス')
-    plt.legend(loc='best') # 凡例をいい感じの位置に配置する
+    plt.title(f'{hoseRaceResults[0].Hose.name} \n RPCI x ave-1F マトリクス')
     plt.xlim(40, 70)
     plt.ylim(10, 14)
     plt.savefig('test_hoseRaceResult.png', dpi=300)
+    plt.show()
 
 if __name__ == "__main__":
     main()
